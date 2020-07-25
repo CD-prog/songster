16176bab19f4c64c162bb2b43442595c416d0109
CD-prog <61663007+CD-prog@users.noreply.github.com>
2020-07-25 13:33:10 -0500

    Scaffolds application

---
+++ app.js
@@ -0,0 +1,61 @@
+const mysql = require('mysql')
+const inquirer = require('inquirer')
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: 3306,
+    user:'root',
+    password:'Claudiu_39',
+    database:'top_songsDB'
+})
+
+connection.connect(err =>{
+    if(err) throw err
+    console.log(`Connect on thread ${connection.threadId}`)
+    initialPrompts()
+})
+function initialPrompts(){
+    inquirer.prompt([
+        {
+            name:'action',
+            message: 'What do you wanna do?',
+            type:'list',
+            choices:['ARTIST SEARCH','MULTI SEARCH','RANGE SEARCH','SONG SEARCH','EXIT']
+        }
+    ]).then(answer => {
+        switch(answer.action){
+            case 'ARTIST SEARCH':
+                artistSearch()
+                break
+            case 'MULTI SEARCH':
+                multiSearch()
+                break
+            case 'RANGE SEARCH':
+                rangeSearch()
+                break
+            case 'SONG SEARCH':
+               songSearch()
+                break
+            default:
+                connection.end()
+                process.exit()
+        }
+    })
+}
+
+function artistSearch(){
+    console.log('Searching artist...')
+    initialPrompts()    
+}
+function multiSearch(){
+    console.log('MultiSearch...')
+    initialPrompts()
+}
+function rangeSearch(){
+    console.log('Searching rangeSearch...')
+    initialPrompts()
+}
+function songSearch(){
+    console.log('Searching song...')
+    initialPrompts()
+}
